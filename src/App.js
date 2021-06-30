4748d05aede25885dc7f1e0acc22a35c960b79ad
Facundo <bacigalupofacundo@gmail.com>
2021-06-30 15:12:20 -0300

    Added focus on car section, changed styles to match stellantis brand

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import QuestionSection from "./components/QuestionSection";
 import CarsSection from "./components/CarsSection";
 import "antd/dist/antd.css";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { readExcel } from "./service/ExcelToData";
+import { Button } from "antd";
+
+const URL_FORM = "https://forms.gle/QkL5GUEiTaAtY7828";
 
 function App() {
   const [cars, setCars] = useState([]);
+
+  const carSectionRef = useRef(null);
+
+  useEffect(() => {
+    if (cars.length !== 0) {
+      carSectionRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [cars]);
 
   const loadCars = (answers) => {
     readExcel().then((excelCars) => recommendCars(excelCars, answers));
   };
 
   const recommendCars = (cars, answers) => {
-    console.log(cars);
     const carsFilteredByTypeAndFuel = cars.filter(
       (car) =>
         answers.CarType.includes(car["Tipo Auto"].toUpperCase()) &&
@@ -46,7 +56,21 @@ function App() {
       <div className="logo">
         <h2>CarMatch</h2>
       </div>
-      <CarsSection cars={cars} />
+      <Button
+        type="primary"
+        className="link-to-form-button"
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.href = URL_FORM;
+        }}
+      >
+        <b>Antes de irte, déjanos tu comentario!</b>
+      </Button>
+      {cars.length !== 0 && (
+        <div className="car-section" ref={carSectionRef}>
+          <CarsSection cars={cars} />{" "}
+        </div>
+      )}
     </div>
   );
 }
